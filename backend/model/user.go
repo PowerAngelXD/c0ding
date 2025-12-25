963b7b40b278e721967480d13a0090fd0e15e5c4
powerangelXD <692732163@qq.com>
2025-12-25 15:21:43 +0800

    refactor: Add json tag

--- backend/model/user.go
+++ backend/model/user.go
@@ -1,34 +1,34 @@
 package model
 
 type User struct {
-	ID       uint
-	UserName string
-	Password string
+	ID       uint   `json:"id" gorm:"primaryKey"`
+	UserName string `json:"userName" gorm:"unique"`
+	Password string `json:"-"`
 
 	// 用户等级
-	Level uint
+	Level uint `json:"level"`
 	// 用户经验
-	Exp uint
+	Exp uint `json:"exp"`
 	// 用户获得的赞
-	RecvLikes uint
+	RecvLikes uint `json:"recvLikes"`
 	// 关注该用户的用户数量
-	RecvSubscription uint
+	RecvSubscription uint `json:"recvSubscription"`
 }
 
 type Like struct {
 	// 主键索引
-	Index uint
+	Index uint `json:"index" gorm:"primaryKey"`
 	// 用户ID
-	UserID uint
+	UserID uint `json:"userId"`
 	// 被点赞的帖子ID
-	PostID uint
+	PostID uint `json:"postId"`
 }
 
 type View struct {
 	// 主键索引
-	Index uint
+	Index uint `json:"index" gorm:"primaryKey"`
 	// 用户ID
-	UserID uint
+	UserID uint `json:"userId"`
 	// 被浏览的帖子ID
-	PostID uint
+	PostID uint `json:"postId"`
 }
